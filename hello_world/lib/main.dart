481bf60fd4b90760e01d6b90578b2959818b1a99
JonasSatkauskas <satkusmedia@gmail.com>
2021-10-04 17:14:55 +0300

    Understanding widgets

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -4,21 +4,38 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Labas vakaras'),
-        ),
+      title: 'Joes App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Hello World!')),
+        body: Column(
+          children: <Widget>[TestWidget(), TestWidget(), TestWidget()],
+        ));
+  }
+}
+
+class TestWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello world!');
   }
 }
